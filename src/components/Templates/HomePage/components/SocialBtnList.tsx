f379328ece0f380d578d34fe1937d69659a71172
Leandro Rangel <leandrorangelm@gmail.com>
2026-01-28 22:52:47 -0300

    feat: ProfileCard component
    - update SocialBtnList style

--- src/components/Templates/HomePage/components/SocialBtnList.tsx
+++ src/components/Templates/HomePage/components/SocialBtnList.tsx
@@ -4,24 +4,22 @@ import { socialLinks } from "@/data/sociaLinks";
 import { getIconByKey } from "@/utils/getIconsByKey";
 
 const SocialBtnList = () => {
-  if (!socialLinks) <></>;
+  if (!socialLinks || socialLinks?.length < 1) null;
 
   return (
-    <div className="flex flex-col justify-center gap-2 sm:flex-row lg:justify-start">
-      <div className="flex items-center gap-2.5">
-        {socialLinks?.map(({ name, href, iconKey }) => {
-          const Icon = getIconByKey(iconKey);
-          if (!Icon) return null;
+    <div className="flex gap-2.5">
+      {socialLinks?.map(({ name, href, iconKey }) => {
+        const Icon = getIconByKey(iconKey);
+        if (!Icon) return null;
 
-          return (
-            <Button key={name} variant="outline" size="icon" asChild>
-              <Link href={href} target="_blank" aria-label={name}>
-                <Icon className="stroke-muted-foreground w-5 h-5" />
-              </Link>
-            </Button>
-          );
-        })}
-      </div>
+        return (
+          <Button key={name} variant="ghost" size="icon" asChild>
+            <Link href={href} target="_blank" aria-label={name}>
+              <Icon className="stroke-muted-foreground w-5 h-5" />
+            </Link>
+          </Button>
+        );
+      })}
     </div>
   );
 };
