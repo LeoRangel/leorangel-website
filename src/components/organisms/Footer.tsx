77bd75be9f5a23a9b5c06a83d573463f1e0ceda5
Leandro Rangel <leandrorangelm@gmail.com>
2026-01-29 17:32:14 -0300

    feat: new footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,12 +1,22 @@
 import { Separator } from "@ui/separator";
+import { SocialBtnList } from "./SocialBtnList";
+import { Container } from "@atoms/Container";
 
 export default function Footer() {
   return (
-    <footer className="w-full mt-10">
+    <footer className="w-full flex flex-col bg-gray-50 dark:bg-gray-900">
       <Separator />
-      <div className="text-center text-sm text-muted-foreground py-4">
-        © {new Date().getFullYear()} | Developed by Leandro Rangel
-      </div>
+      <Container variant="fullMobileConstrainedPadded" className="w-full">
+        <div className="w-full flex flex-col md:flex-row md:items-center justify-between my-6">
+          <div className="text-center text-sm text-muted-foreground py-4">
+            © {new Date().getFullYear()} - Developed by Leandro Rangel
+          </div>
+
+          <div className="flex gap-3 justify-center">
+            <SocialBtnList />
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 }
