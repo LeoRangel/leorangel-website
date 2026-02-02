e87fd7972c58b135fb7ddfc95fbb806259c2a11f
Leandro Rangel <leandrorangelm@gmail.com>
2026-02-02 12:57:28 -0300

    fix: add fallback hostname for images.remotePatterns in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: process.env.NEXT_PUBLIC_WORDPRESS_API_HOSTNAME,
+        hostname:
+          process.env.NEXT_PUBLIC_WORDPRESS_API_HOSTNAME ||
+          "my-website-wp-cms.local",
         port: "",
+        pathname: "/**",
       },
     ],
   },
