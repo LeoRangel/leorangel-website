95850d52eacdf2179a21e058b8af304643f6ba9c
Leandro Rangel <leandrorangelm@gmail.com>
2026-02-04 22:48:58 -0300

    refactor: projects qtd const

--- src/queries/projects/ProjectsQuery.ts
+++ src/queries/projects/ProjectsQuery.ts
@@ -3,7 +3,7 @@ import gql from "graphql-tag";
 const PROJECTS_TO_SHOW_QTD = 6;
 
 export const ProjectsQuery = gql`
-  query ProjectsQuery($projectQtd: Int = 6) {
+  query ProjectsQuery($projectQtd: Int = ${PROJECTS_TO_SHOW_QTD}) {
     projects(
       first: $projectQtd
       where: { orderby: { field: DATE, order: DESC } }
